7684c73c9b1a33a0bc99b0026c32b0a84560f3fc
Paul McMurdie <joey711@gmail.com>
2011-10-26 09:41:50 -0700

    Ver 0.3.27. UniFrac Doc, transpose bug fix

--- R/transpose-methods.r
+++ R/transpose-methods.r
@@ -1,14 +1,18 @@
 ##############################################################################
-# Extension of the transpose function in R.
+# Extension of the transpose function in R for phyloseq objects.
+# This is really only applied to the otuTable category.	
 #
 setGeneric("t")
 setMethod("t", signature("otuTable"), function(x){
-	new("otuTable", t(x@.Data),
-		speciesAreRows=!speciesarerows(x))
+	#new("otuTable", t(x@.Data), speciesAreRows = (!speciesAreRows(x)))
+	x <- otuTable( t(as(x, "matrix")), speciesAreRows=(!speciesAreRows(x)) )
+	return(x)
 })
 ##############################################################################
-
-
-
+setMethod("t", signature("phyloseqFather"), function(x){
+	x@otuTable <- t( otuTable(x) )
+	return(x)
+})
+##############################################################################
 
 
