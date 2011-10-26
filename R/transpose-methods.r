9451cf7520282e788608b05c8ce56b9816fca473
Paul J. McMurdie <mcmurdie@stanford.edu>
2011-10-26 11:56:20 -0700

    ver 0.3.38 transpose bugfix plus doc
    t() wasn't working properly because it wasn't exported. Its
    functionality could be recovered with phyloseq:::t(). Since this is
    intended to be a user-accessible method, as well as used internally, it
    is now exported and fully documented.

--- R/transpose-methods.r
+++ R/transpose-methods.r
@@ -1,14 +1,32 @@
 ##############################################################################
-# Extension of the transpose function in R for phyloseq objects.
-# This is really only applied to the otuTable category.	
-#
+#' Extension of the (base) transpose function for otuTable
+#'
+#' Only the otuTable of a more complex object is transposed.	
+#'
+#' @usage t(x)
+#'
+#' @param x An \code{otuTable}, or a higher-order class that contains an \code{otuTable}.
+#'
+#' @return The class of the object returned by \code{t} matches
+#' the class of the argument, \code{x}. The \code{otuTable} is now
+#' transposed, and \code{speciesAreRows} is toggled.
+#'
+#' @name t
+#' @rdname transpose-methods
+#' @docType methods
+#' @export
+#' @examples #
 setGeneric("t")
+#' @aliases t,otuTable-method
+#' @rdname transpose-methods
 setMethod("t", signature("otuTable"), function(x){
 	#new("otuTable", t(x@.Data), speciesAreRows = (!speciesAreRows(x)))
 	x <- otuTable( t(as(x, "matrix")), speciesAreRows=(!speciesAreRows(x)) )
 	return(x)
 })
 ##############################################################################
+#' @aliases t,phyloseqFather-method
+#' @rdname transpose-methods
 setMethod("t", signature("phyloseqFather"), function(x){
 	x@otuTable <- t( otuTable(x) )
 	return(x)
