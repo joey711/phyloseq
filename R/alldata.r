535a6b8e01aaf55c723816207526ac2a0c59526a
Paul J. McMurdie <mcmurdie@stanford.edu>
2012-02-21 10:26:19 -0800

    v0.99.22 fixed erroneous journal name in GlobalPatterns dataset doc
    The first sentence of the GlobalPatterns dataset description says that
    it was published in Nature, when in fact it was published in PNAS. The
    formal citation later in the documentation was and still is correct.
    Thanks to Greg Caporaso for pointing-out this typo.

--- R/alldata.r
+++ R/alldata.r
@@ -227,7 +227,7 @@ NA
 ################################################################################
 #' (Data) Global patterns of 16S rRNA diversity at a depth of millions of sequences per sample (2011)
 #'
-#' Published in Nature in early 2011. This work compared the microbial 
+#' Published in PNAS in early 2011. This work compared the microbial 
 #' communities from 25 environmental samples and three known ``mock communities''
 #' -- a total of 9 sample types -- at a depth averaging 3.1 million reads per sample.
 #' Authors were able to reproduce diversity patterns seen in many other 
