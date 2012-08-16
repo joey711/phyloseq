3e905cf17af0dec3be7bf9f0d7021c0726e778a9
Paul J. McMurdie <mcmurdie@stanford.edu>
2012-08-15 18:07:01 -0700

    Fixed references to readTree, now deprecated: read_tree

--- inst/tests/test-IO.R
+++ inst/tests/test-IO.R
@@ -170,9 +170,9 @@ test_that("the import_biom and import(\"biom\", ) syntax give same result", {
 	expect_that(x1, is_identical_to(x2))
 })
 ################################################################################
-# readTree tests
-test_that("The readTree function works as expected:", {
-	GPNewick <- readTree(system.file("extdata", "GP_tree_rand_short.newick.gz", package="phyloseq"))
+# read_tree tests
+test_that("The read_tree function works as expected:", {
+	GPNewick <- read_tree(system.file("extdata", "GP_tree_rand_short.newick.gz", package="phyloseq"))
 	expect_that(GPNewick, is_a("phylo"))
 	expect_that(ntaxa(GPNewick), equals(length(GPNewick$tip.label)))
 	expect_that(ntaxa(GPNewick), equals(500))
@@ -180,10 +180,10 @@ test_that("The readTree function works as expected:", {
 	expect_that(taxa_names(GPNewick), is_identical_to(GPNewick$tip.label))	
 	# Now read a nexus tree... 
 	# Some error-handling expectations
-	expect_that(readTree("alskflsakjsfskfhas.akshfaksj"), gives_warning()) # file not exist
+	expect_that(read_tree("alskflsakjsfskfhas.akshfaksj"), gives_warning()) # file not exist
 	not_tree <- system.file("extdata", "esophagus.good.groups.gz", package="phyloseq")
-	expect_that(readTree(not_tree), is_a("NULL")) # file not a tree, gives NULL
-	expect_that(readTree(not_tree, TRUE), throws_error()) # file not a tree, check turned on/TRUE
+	expect_that(read_tree(not_tree), is_a("NULL")) # file not a tree, gives NULL
+	expect_that(read_tree(not_tree, TRUE), throws_error()) # file not a tree, check turned on/TRUE
 })
 ################################################################################
 # Next import function's test...
