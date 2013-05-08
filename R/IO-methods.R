bc1ea38243917998dc292c9be6a5c1c4d78673ae
Paul J. McMurdie <mcmurdie@stanford.edu>
2013-05-07 17:50:07 -0700

    preliminary microbio.me/qiime API. incomplete
    Missing unit tests, example data, and some kind of issue with tar.gz
    files but not .zip files. The server appears to have both kinds...

--- R/IO-methods.R
+++ R/IO-methods.R
@@ -1900,3 +1900,173 @@ build_tax_table = function(taxlist){
 ################################################################################
 ################################################################################
 ################################################################################
+#' Download and import directly from microbio.me/qiime
+#' 
+#' This function is for accessing microbiome datasets from the
+#' \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}
+#' public repository from within R.
+#' I haven't yet figured out how to list the available studies at
+#' \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}
+#' from within R
+#' (and I don't know if they have made this possible),
+#' but I have provided illustrated instructions for finding 
+#' details about studies you might want to download and explore,
+#' as well as the FTP address that you will need. 
+#' Note that you likely need to create an account at
+#' \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}
+#' in order to explore the studies they have available for download.
+#' Please see a detailed tutorial for further help finding the FTP address 
+#' of the data that you want. Also note that this function will work
+#' on the full address to any \code{.zip} zipped file with microbiome
+#' data organized with the same naming conventions and file types as
+#' \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}.
+#' Please also see the 
+#' \href{http://joey711.github.io/phyloseq/download-microbio.me.html}{microbio_me_qiime tutorial}
+#' 
+#' @param zipftp (Required). A character string that is the full URL
+#'  path to a zipped file that follows the file naming conventions used by 
+#'  \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}.
+#'  Alternatively, you can simply provide the study number,
+#'  as a single-length \code{\link{numeric}},
+#'  and this function will complete the remainder of the ftp URL hosted at
+#'  \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}.
+#'  For example, instead of the full URL string, 
+#'  \code{"ftp://thebeast.colorado.edu/pub/QIIME_DB_Public_Studies/study_494_split_library_seqs_and_mapping.zip"}, you could simply provide \code{494} as the `zipftp` argument.
+#'  Note that this is internally dispatching based on the class,
+#'  so \code{"494"} will not work (just leave off the quotes for study number-only).
+#'  
+#'  @param ext (Optional). A \code{\link{character}} string of the expected
+#'   file extension, which also indicates the compression type,
+#'   if \code{zipftp} is a study number instead of the full path. 
+#'   Note that this argument has no effect if \code{zipftp} is the full path,
+#'   in which case the file extension is read directly from the downloaded file.
+#'  
+#'  @param ... (Optional, for advanced users). Additional arguments passed to 
+#'   \code{\link{download.file}}. This is mainly for non-standard links to
+#'   resources (in this case, a zipped file) that are not being hosted by
+#'   \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}.
+#'   If you are using a FTP address or study number from their servers,
+#'   then you shouldn't need to provide any additional arguments.
+#' 
+#' @return
+#'  A \code{\link{phyloseq-class}} object if possible, a component if only a 
+#'  component could be imported, or \code{NULL} if nothing could be imported
+#'  after unzipping the file. Keep in mind there is a specific naming-convention
+#'  that is expected based on the current state of the
+#'  \href{http://www.microbio.me/qiime/index.psp}{microbio.me/qiime}
+#'  servers. Several helpful messages are \code{\link{cat}}ted to standard out
+#'  to help let you know the ongoing status of the current 
+#'  download and import process.
+#'
+#' @seealso
+#'  See \code{\link{download.file}} and \code{\link{url}}
+#'  for details about URL formats --
+#'  including local file addresses -- that might work here.
+#'  
+#'  \code{\link{import_biom}}
+#'  
+#'  \code{\link{import_qiime}}
+#'  
+#' @export
+#' @examples
+#' # This should return TRUE on your system if you have internet turned on
+#' # and a standard R installation. Indicates whether this is likely to
+#' # work on your system for a URL or local file, respectively.
+#' capabilities("http/ftp"); capabilities("fifo")
+#' # A real example 
+#' # # Smokers dataset
+#' # smokezip = "ftp://thebeast.colorado.edu/pub/QIIME_DB_Public_Studies/study_524_split_library_seqs_and_mapping.zip"
+#' # smokers1 = microbio_me_qiime(smokezip)
+#' # # Alternatively, just use the study number
+#' # smokers2 = microbio_me_qiime(524)
+#' # identical(smokers1, smokers2)
+microbio_me_qiime = function(zipftp, ext=".zip", ...){
+	# zipftp = "ftp://thebeast.colorado.edu/pub/QIIME_DB_Public_Studies/study_721_split_library_seqs_and_mapping.tgz"
+	# zipftp = "~/Downloads/study_721_split_library_seqs_and_mapping.tgz"
+	# zipftp = "~/Downloads/study_721_split_library_seqs_and_mapping.zip"
+	# Define naming convention
+	front = "ftp://thebeast.colorado.edu/pub/QIIME_DB_Public_Studies/study_"	
+	if( inherits(zipftp, "numeric") ){
+		# If study number instead of string,
+		# create the ftp URL using ext and convention
+		back  = paste0("_split_library_seqs_and_mapping", ext)
+		zipftp = paste0(front, zipftp, back)
+	} else {
+		# Determine file extension from the file path itself
+		ext = substring(zipftp, regexpr("\\.([[:alnum:]]+)$", zipftp)[1])
+		back  = paste0("_split_library_seqs_and_mapping", ext)
+	}
+	# Check if zipftp is clearly an externally located file, ftp, http, etc.
+	externprefixes = c("http://", "https://", "ftp://")
+	prefix = regexpr("^([[:alnum:]]+)\\://", zipftp)
+	if( substr(zipftp, 1, attr(prefix, "match.length")[1]) %in% externprefixes ){
+		# If external, then create temporary file and download
+		zipfile = tempfile()
+		download.file(zipftp, zipfile, ...)
+	} else {
+		# Else it is a local zipfile
+		zipfile = zipftp
+	}
+	import_dir = tempdir()
+	# Unpack to the temporary directory using unzip or untar
+	if( ext == ".zip" ){
+		filelist = unzip(zipfile, list=TRUE)
+		#mapfile = filelist[grep("mapping_file.txt", filelist, fixed=TRUE)]
+		#biomfile = filelist[grep(".biom", filelist, fixed=TRUE)]		
+		# Unzip the zipfile to the new temp dir
+		#unzip(zipfile, files=c(mapfile, biomfile), exdir=import_dir)	
+		unzip(zipfile, exdir=import_dir)	
+	} else if( ext %in% c("tar.gz", ".tgz", ".gz", ".gzip", ".bzip2", ".xz") ){
+		# untar the tarfile to the new temp dir
+		filelist = untar(zipfile, list=TRUE)
+		#mapfile = filelist[grep("mapping_file.txt", filelist, fixed=TRUE)]
+		#biomfile = filelist[grep(".biom", filelist, fixed=TRUE)]
+		untar(zipfile, exdir=import_dir)
+	} else {
+		# The compression format was not recognized. Provide informative error msg.
+		stop("Could not determine the compression type. Expected extensions are (mostly) .zip or .tgz")
+	}
+	# Use the apparent file naming convention for microbio.me/qiime
+	# as the de facto guide for this API
+	studyname = gsub(back, "", basename(zipftp), fixed=TRUE)
+	# Check if biom file present, and parse if it is.
+	biomfile = paste0(import_dir, "/", studyname, "_closed_reference_otu_table.biom")
+	imported_objects = vector("list")
+	if( file.exists(biomfile) ){
+		cat("Found biom-format file, now parsing it... \n")
+		biom = import_biom(biomfile, parseFunction = parse_taxonomy_greengenes)
+		cat("Done parsing biom... \n")
+		imported_objects = c(imported_objects, list(biom))
+	}
+	# Check if sample_data (qiime mapping) file present, and parse if it is.
+	sdfile = paste0(import_dir, "/", studyname, "_mapping_file.txt")
+	if( file.exists(sdfile) ){
+		sample_metadata = import_qiime_sample_data(sdfile)
+		imported_objects = c(imported_objects, list(sample_metadata))
+	}
+	# Check success, notify user, and return.
+	if( length(imported_objects) > 1 ){
+		# If there are more than one imported objects, merge them and return
+		cat("Merging the imported objects... \n")
+		physeq = do.call("merge_phyloseq", imported_objects)
+		if( inherits(physeq, "phyloseq") ){
+			cat("Successfully merged, phyloseq-class created. \n Returning... \n")	
+		}
+		return(physeq)
+	} else if( length(imported_objects) == 1 ){
+		cat("Note: only on object in the zip file was imported. \n")
+		cat("It was ", class(imported_objects[[1]]), " class. \n")
+		return(imported_objects[[1]])
+	} else {
+		cat("PLEASE NOTE: No objects were imported. \n", 
+				"You chould check the zip file, \n",
+				"as well as the naming conventions in the zipfile \n",
+				"to make sure that they match microbio.me/qiime. \n",
+				"Instead returning NULL... \n")
+		return(NULL)
+	}
+}
+################################################################################
+################################################################################
+################################################################################
+################################################################################
