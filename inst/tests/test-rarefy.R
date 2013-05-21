2cf90c706965118804f8c9847b2604e912d8d676
Paul J. McMurdie <mcmurdie@stanford.edu>
2013-05-21 15:16:53 -0700

    v1.5.16 Upgrades to rarefy_even_depth() function
    More user feedback about process.
    New argument for setting RNG seed, with a default value (reproducible
    by default).
    Reports the RNG seed being used for random subsampling, encouraging
    users to record this for reproducibility.
    Trims empty samples/OTUs automatically, and reports this to standard
    out.

---
+++ inst/tests/test-rarefy.R
@@ -0,0 +1,39 @@
+################################################################################
+# Use testthat to test phyloseq transformation functions/methods
+################################################################################
+library("phyloseq"); library("testthat")
+# # # # TESTS!
+################################################################################
+# rarefy_even_depth
+################################################################################
+data("GlobalPatterns")
+set.seed(711) # The random seed for randomly selecting subset of OTUs
+randoOTUs = sample(taxa_names(GlobalPatterns), 100, FALSE)
+GP100 = prune_taxa(randoOTUs, GlobalPatterns)
+# The default rng seed is being implied in this call (also 711)
+rGP = rarefy_even_depth(GP100, 1000)
+################################################################################
+# Test that specific OTUs and samples were removed
+################################################################################
+test_that("Test that empty OTUs and samples were automatically pruned", {
+	rmOTU = setdiff(taxa_names(GP100), taxa_names(rGP))
+	expect_equal(length(rmOTU), 20L)
+	expect_equal(rmOTU[1:5], c("534601", "408325", "325564", "8112", "571917"))
+	expect_true(taxa_names(GP100)[taxa_sums(GP100) <= 0] %in% rmOTU)
+	expect_true(all(taxa_sums(rGP) > 0))
+	rmsam = setdiff(sample_names(GP100), sample_names(rGP))
+	expect_equal(length(rmsam), 12L)
+	expect_equal(rmsam[1:5], c("M11Fcsw", "M31Tong", "M11Tong", "NP2", "TRRsed1"))
+	expect_true(all(sample_sums(rGP) > 0))
+})
+################################################################################
+# Test specific values. Should be reproducible, and you set the seed.
+################################################################################
+test_that("Test values", {
+	expect_equal(as(otu_table(rGP)[1, 3:10], "vector"), rep(0, 8))
+	expect_equal(as(otu_table(rGP)[2, 1:10], "vector"), c(rep(0, 9), 1))
+	expect_equal(as(otu_table(rGP)[3, 8:12], "vector"), c(883, 962, 59, 9, 29))
+	expect_equal(as(otu_table(rGP)[70:78, 4], "vector"),
+							 c(707, 3, 0, 2, 0, 8, 157, 2, 0))
+})
+################################################################################
