002f08d390423d4e13a2cafb7f88fc81e561dcad
Paul J. McMurdie <mcmurdie@stanford.edu>
2013-10-24 00:15:03 +0000

    phyloseq 1.7.3 - Added DESeq2 interface function, `phyloseq_to_deseq2`, added DESeq2 vignette, all vignettes now .Rmd, reformatted NEWS
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/phyloseq@82087 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/installer.R
+++ inst/scripts/installer.R
@@ -1,21 +1,21 @@
 thisVer <- getRversion()
-if(!compareVersion(as.character(thisVer), "2.15.0") >=0)
-  stop("R v2.15 is required")
-# Define dependencies in installer  
-cran.nms <- c("ape", "doParallel", "foreach", "ggplot2", "igraph", "picante", "vegan", "RJSONIO", "plyr")
-bioc.nms <- c("multtest", "genefilter")
-##  cran
-install.packages(cran.nms)
-## BioC
+if(!compareVersion(as.character(thisVer), "3.0.0") >=0)
+  stop("R v3.0 or greater is required")
+## Step 1
+## Install the release version from BioC
 source("http://bioconductor.org/biocLite.R")
-biocLite(bioc.nms)
-## use devtools to install latest version of phyloseq from GitHub
-if (!require('devtools'))
-  install.packages('devtools')  # needs Curl for RCurl
-library("devtools")
-pkgs <- list(joey711 = c("phyloseq"))
-for (repo in names(pkgs)) {
-  for (pkg in pkgs[[repo]]) install_github(pkg, repo)
+biocLite("phyloseq")
+## You may also want to go further and install the latest devel from BioC,
+## or further still, from GitHub (the most recent).
+## Step 2 
+## Install from BioC-devel
+devel = "http://bioconductor.org/packages/2.14/bioc"
+biocLite("phyloseq", siteRepos=devel, suppressUpdates=TRUE, type="source")
+## Step 3
+## Use devtools::install_github to install latest version of phyloseq from GitHub
+if(!require("devtools", quietly=TRUE)){
+  # Note: needs Curl for RCurl
+  install.packages("devtools")
 }
-
-
+library("devtools")
+devtools::install_github("phyloseq", "joey711")
